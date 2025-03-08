3118197233510ee4e9946f5c2a28c0f3e9088225
Oumou30 <oumousadio328@gmail.com>
2025-03-08 14:01:40 +0100

    Mise à jour du portfolio et Ajout de la vidéo motsCroisses avec Git LFS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import MorpionPage from './components/MorpionPage';
+import MotsCroisses from './components/MotsCroisses';
+import ClubSportifPage from './components/ClubSportifPage';
+import DechetsPage from './components/DechetsPage';
+import EmpruntsPage from './components/EmpruntsPage';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import RoadsPage from './components/RoadsPage';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <Router>
+      <Header />
+      <Routes>
+        {/* Page d'accueil complète */}
+        <Route path="/" element={
+          <div>
+            <Hero />
+            <About />
+            <Projects />
+            <Contact />
+          </div>
+        } />
+
+        {/* Routes propres pour chaque section */}
+        <Route path="/apropos" element={<About />} />
+        <Route path="/portfolio" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+
+        {/* Pages spécifiques des projets */}
+        <Route path="/morpion" element={<MorpionPage />} />
+        <Route path="/motsCroisses" element={<MotsCroisses />} />
+        <Route path="/club-sportif" element={<ClubSportifPage />} />
+        <Route path="/gestion-dechets" element={<DechetsPage />} />
+        <Route path="/gestion-emprunts" element={<EmpruntsPage />} />
+        <Route path="/guinea-roads" element={<RoadsPage />} />
+      </Routes>
+    </Router>
   );
 }
 
